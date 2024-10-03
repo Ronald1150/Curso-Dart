3f3502771169debfb50c74c9eb21a48cbaa70719
964490036 <rv622170@gmail.com>
2024-10-03 12:40:27 -0500

    clase_Dart 03/10/2024

---
+++ main.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  /*String text = "Jhon";
+  int edad = 25;
+  bool isVerdadero = true;
+  double decimal = 20.0;
+  List<String> animales = ["perro,gato,loro"];
+  print({text, edad, isVerdadero, decimal, animales});
+}*/
+//Operadores aritmetricos
+  int a = 10;
+  int b = 20;
+  int suma = a + b;
+  int resta = a - b;
+  int multi = a * b;
+  double divicion = a / b;
+  print(a + b);
+}
